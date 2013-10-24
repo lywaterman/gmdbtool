143085dacad9b84b382225bbb96245bbbd2dc274
ubuntu-server <liuyang@ubuntu-server-64>
2013-10-24 11:16:54 +0800

    add supervisor ctl

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ from pymongo import MongoClient
 
 import json
 
+from datetime import datetime
+
 settings = {'debug' : True,
             'template_path':os.path.join(os.path.dirname(__file__), "templates"),
             'static_path':os.path.join(os.path.dirname(__file__), "static")
@@ -58,26 +60,78 @@ class ChangeGoldHandler(tornado.web.RequestHandler):
         print oid
         print newgold
 
-        client = MongoClient('localhost', 18188)
-        db = client.flower
-        db.info.update({u"oid": oid}, {u"$set":{u"gold": newgold}})
-
-        doc = db.info.find_one({u"oid":oid}, {u"_id":0})
-
-        if doc == None:
-            oid = 0
-
-        jsoncode = json.dumps(doc, sort_keys=True, indent=4, ensure_ascii=False)
-        htmlcode = highlight(jsoncode, JsonLexer(encoding='utf-8'), HtmlFormatter(encoding='utf-8', cssclass="highlight"))
-
-        self.render("infodiv.html", body=htmlcode, oid=oid)
+        make_change(self, oid, "gold", newgold)
+            
+def make_change(handler, oid, key, value):
+    if key == 'oid' or key == 'id' or key == 'idtype':
+        handler.render("infodiv.html", body="key不能是oid, id, idtype", oid=oid)
+        return
         
+    client = MongoClient('localhost', 18188)
+    db = client.flower
+    ##key = unicode(key, "utf-8")
+    ## 保存gm操作
+    doclast = db.info.find_one({u"oid":oid}, {u"_id":0})
+    doclast[u'gmop'] = {u'key': key, u'value':value, u'time':datetime.now()}
+    db.gmop.insert(doclast)
+    
+    db.info.update({u"oid": oid}, {u"$set":{key : value}})
+    
+    doc = db.info.find_one({u"oid":oid}, {u"_id":0})
+    
+    if doc == None:
+        oid = 0
+        
+    jsoncode = json.dumps(doc, sort_keys=True, indent=4, ensure_ascii=False)
+    htmlcode = highlight(jsoncode, JsonLexer(encoding='utf-8'), HtmlFormatter(encoding='utf-8', cssclass="highlight"))
+    
+    handler.render("infodiv.html", body=htmlcode, oid=oid)
 
+    
+class ChangeAnyHandler(tornado.web.RequestHandler):
+    def post(self):
+        oid = int(self.get_argument('oid'))
+        key = self.get_argument('key')
+        value = int(self.get_argument('value'))
+
+        print oid
+        print key
+        print value
+
+        make_change(self, oid, key, value)
+        
+class ChangeAnyBooleanHandler(tornado.web.RequestHandler):
+   def post(self):
+       oid = int(self.get_argument('oid'))
+       key = self.get_argument('key')
+       value = self.get_argument('value') == "true" and True or False
+
+       print oid
+       print key
+       print value
+
+       make_change(self, oid, key, value)
+       
+class ChangeAnyStringHandler(tornado.web.RequestHandler):
+   def post(self):
+       oid = int(self.get_argument('oid'))
+       key = self.get_argument('key')
+       value = self.get_argument('value')
+
+       print oid
+       print key
+       print value
+
+       make_change(self, oid, key, value)
+       
 application = tornado.web.Application([
         (r"/", MainHandler),
         (r"/login", LoginHandler),
         (r"/change_gold", ChangeGoldHandler),
         (r"/query_info", QueryInfoHandler),
+        (r"/change_any", ChangeAnyHandler),
+        (r"/change_any_boolean", ChangeAnyBooleanHandler),
+        (r"/change_any_string", ChangeAnyStringHandler)
         ], **settings)
 
 if __name__ == '__main__':
